85d6cb42ad3b4d9d574361c9d955d0c8272e48d4
leader22 <y.sugiura.0316@gmail.com>
2016-11-11 15:13:31 +0900

    add styling

--- src/view.js
+++ src/view.js
@@ -1,20 +1,70 @@
+const styles = `
+.App {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  min-height: 250px;
+
+  margin: 20px auto;
+
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  font-family: impact;
+}
+.App-noteWrap {
+  margin-right: 50px;
+}
+.App-note {
+  color: #f00;
+  font-size: 120px;
+}
+.App-detune {
+  font-size: 40px;
+}
+.App-cent {
+  color: #0f0;
+  font-size: 20px;
+}
+`;
+
 class View {
   constructor(doc) {
-    this.pitchEl  = doc.getElementById('pitch');
-    this.noteEl   = doc.getElementById('note');
-    this.detuneEl = doc.getElementById('detune');
+    this.el = doc.querySelector('#jsApp');
+
+    const style = doc.createElement('style');
+    const css = doc.createTextNode(styles);
+    style.appendChild(css);
+    doc.head.appendChild(style);
   }
 
   render(store) {
+    let dom = '';
+
     if (store.pitch === -1) {
-      this.pitchEl.innerText  = '';
-      this.noteEl.innerText   = 'N/A';
-      this.detuneEl.innerText = '';
+      dom = `
+        <div>
+          <span class="App-note">N/A</span>
+        </div>
+      `;
     } else {
-      this.pitchEl.innerText  = store.pitch|0;
-      this.noteEl.innerText   = store.noteStr;
-      this.detuneEl.innerText = store.centDir + store.centGap;
+      dom = `
+        <div class="App-noteWrap">
+          <span class="App-note">${store.noteStr}</span>
+        </div>
+        <div>
+          <div>
+            <span class="App-detune">${store.pitch|0}Hz</span>
+          </div>
+          <div>
+            <span class="App-cent">${store.centDir}${store.centGap}cents</span>
+          </div>
+        </div>
+      `;
     }
+
+    this.el.innerHTML = dom;
   }
 }
 
